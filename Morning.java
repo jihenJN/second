53767b3118c03af61a02df1764377b528c7d2239
jihennasri <jihene.nassri@gmail.com>
2022-08-04 08:49:49 +0200

    modification text

--- Morning.java
+++ Morning.java
@@ -1,5 +1,5 @@
 public class Morning{
     public static void public static void main(String[] args) {
-        System.out.println("good day");
+        System.out.println("good day is today !!");
     }
 }
